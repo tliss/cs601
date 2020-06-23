223aa7f476722579578dd2aaa468de29726f816d
Taylor Liss <tayloraliss@gmail.com>
2020-06-22 23:52:35 -0400

    Finish gallery

--- public/scripts/vue_gallery.js
+++ public/scripts/vue_gallery.js
@@ -3,26 +3,39 @@ Vue.component('Gallery', {
         return {
             selectedImage: 0,
             variants: [
-                { id: 1, image: 'public/images/gallery/1.png' },
-                { id: 2, image: 'public/images/gallery/2.png' },
-                { id: 3, image: 'public/images/gallery/3.png' },
-                { id: 4, image: 'public/images/gallery/4.png' }
+                { id: 1, image: 'public/images/gallery/1.png', caption: "Sitting on the city wall of Xi'an, China in 2011" },
+                { id: 2, image: 'public/images/gallery/2.png', caption: "Kayaking in the Charles river" },
+                { id: 3, image: 'public/images/gallery/3.png', caption: "Working hard during the 2017 BostonHacks hackathon" },
+                { id: 4, image: 'public/images/gallery/4.png', caption: "Practicing Capoeira in at Sinha Capoeira in 2019" }
             ]
         }
     },
     template:`
         <div>
-            <img id="main-image" :src="image">
+            <figure id="vue-figure">
+                <img id="main-image" :src="image">
+                <figcaption id="vue-caption">{{ caption }}</figcaption>
+            </figure>
+
             <div class="thumb-wrapper">
                 <img v-for="(variant, index) in variants" 
                     :src="variants[index].image" 
-                    class="thumbnail"/>
+                    class="thumbnail"
+                    @mouseover="updateImage(index)"/>
             </div>
         </div>
     `,
     computed: {
         image() {
             return this.variants[this.selectedImage].image;
+        },
+        caption() {
+            return this.variants[this.selectedImage].caption;
+        }
+    },
+    methods: {
+        updateImage (index) {
+            this.selectedImage = index;
         }
     }
 })
